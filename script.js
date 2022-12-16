33e027f6c9f92e4ecf1968269c62b22725771807
Ogge81 <115690234+Ogge81@users.noreply.github.com>
2022-12-16 18:07:38 +0100

    script.js, rearanged alot of logic

--- script.js
+++ script.js
@@ -4,76 +4,51 @@ let items = [
     [document.getElementById("scissor"),1,0,"url(scissor.png)"]]
 
 let targets = [
-    [document.getElementById("left"), "yellow", "green", "red"],
-    [document.getElementById("right"), "yellow", "green", "red"]]
+    document.getElementById("left"),
+    document.getElementById("right")]
 
 function over_button( event ){
-    for( let i=0; i<items.length; i++){
-        if( event.target.id === items[i][0].id ){
-            event.target.children[0].textContent = event.target.id;
-            targets[0][0].textContent = event.target.id.toUpperCase();
-            targets[0][0].style.backgroundImage = items[i][3];}}
-}
+    if( event.target.id === "rock" ){
+        targets[0].style.backgroundImage = "url(rock.png)"
+        targets[0].textContent = "ROCK"}
+    if( event.target.id === "paper" ){
+        targets[0].style.backgroundImage = "url(paper.png)"
+        targets[0].textContent = "PAPER"}
+    if( event.target.id === "scissor" ){
+        targets[0].style.backgroundImage = "url(scissor.png)"
+        targets[0].textContent = "SCISSOR"}}
 
 function out_button( event ){
-    if ( event.target.id === "rock" ||
-         event.target.id === "paper" ||
-         event.target.id === "scissor"){
-            targets[1][0].textContent = "";
-            targets[1][0].style.backgroundImage = "none";}
-}
+    if( event.target.children[0].id === "sub" ){
+        for ( let i=0; i<targets.length; i++ ){
+            targets[i].textContent = "";
+            targets[i].style.backgroundImage = "none";
+            targets[i].style.backgroundColor = "white";}}}
 
-function check_winner( event ){
-    let computerHand    = items[ Math.floor(Math.random()*3) ]
-    let winningHand     = items[ computerHand[1] ]
-    let loosingHand     = items[ computerHand[2] ]
-    console.log( computerHand, winningHand, loosingHand )
-    if( event.target.children[0] === "sub"){
-        console.log( computerHand, winningHand, loosingHand )
-    }
-}
+function check_winner( event, computerHand ){
+    document.getElementById("pointR").textContent = String(Number(document.getElementById("pointR").textContent)+1)
+    if( event.target === computerHand[0] ){
+        console.log("Tie")
+        targets[0].style.backgroundColor = "yellow"
+        targets[1].style.backgroundColor = "yellow"}
+    else if( event.target === items[computerHand[1]][0]){
+        document.getElementById("pointC").textContent = String(Number(document.getElementById("pointC").textContent)+1)
+        targets[0].style.backgroundColor = "red"
+        targets[1].style.backgroundColor = "green"}
+    else{
+        document.getElementById("pointP").textContent = String(Number(document.getElementById("pointP").textContent)+1)
+        targets[0].style.backgroundColor = "green"
+        targets[1].style.backgroundColor = "red"}}
 
 function click_button( event ){
-    check_winner( event )
     let computerHand = items[ Math.floor(Math.random()*3) ]
-    console.log( computerHand[0].children, event.target )
-    console.log( event.target.id === computerHand[0].id )
-    if ( event.target.children[0].id === computerHand[0].children[0].id ){
+    targets[1].textContent = computerHand[0].id.toUpperCase();
+    targets[1].style.backgroundImage = computerHand[3];
 
-         targets[1][0].textContent = computerHand[0].id.toUpperCase();
-         targets[1][0].style.backgroundImage = computerHand[3];
+    console.log( computerHand, event.target )
 
-        if( event.target.id === computerHand[0].id ){
-            console.log( "OAVGJORT" )
-            document.getElementById( "left" ).style.backgroundColor = "yellow";
-            document.getElementById( "right" ).style.backgroundColor = "yellow";}
-        else if ( items[ computerHand[1] ][0].id === event.target.id ){
-            console.log( "SPELARE FÖRLORAR" )
-            document.getElementById( "left" ).style.backgroundColor = "red";
-            document.getElementById( "right" ).style.backgroundColor = "green";}
-        else { 
-            console.log( "SPELARE VINNER!" )
-            document.getElementById( "left" ).style.backgroundColor = "green";
-            document.getElementById( "right" ).style.backgroundColor = "red";}
+    check_winner( event, computerHand )}
 
-        //  targets[1].textContent = items[ computerHand ][0].id.toUpperCase();
-        //  targets[1].style.backgroundImage = items[ computerHand ][3];
-         
-        }
-    // if ( items[ computerHand ][0] )
-    console.log( computerHand )
-}
-
-addEventListener("mouseover", function( event ){
-    over_button( event )
-})
-addEventListener("mouseout", function( event ){
-    out_button( event )
-})
-addEventListener("click", function( event ){
-    click_button( event )
-})
-
-// let ctx = document.getCSSCanvasContext("2d", "fooBar", 50, 50 )
-
-// ctx.fillRect(0, 0, 50, 50)
+addEventListener("mouseover", over_button)
+addEventListener("mouseout", out_button)
+addEventListener("click", click_button)
