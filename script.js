fb36f4e65d55753e5ae3031ed79b395b5391068e
Ogge81 <115690234+Ogge81@users.noreply.github.com>
2022-12-16 17:58:13 +0100

    script.js, tryed some new javascript

--- script.js
+++ script.js
@@ -4,18 +4,43 @@ let items = [
     [document.getElementById("scissor"),1,0,"url(scissor.png)"]]
 
 let targets = [
-    document.getElementById("score"),
-    document.getElementById("screen")]
+    document.getElementById("left"),
+    document.getElementById("right")]
 
-addEventListener("mouseover", function( event ){
+function over_button( event ){
     for( let i=0; i<items.length; i++){
         if( event.target.id === items[i][0].id ){
             event.target.children[0].textContent = event.target.id;
             items[items[i][1]][0].children[0].textContent = "Wins!"
             items[items[i][2]][0].children[0].textContent = "Loose.."
-            targets[1].textContent = event.target.id.toUpperCase();
-            targets[1].style.backgroundImage = items[i][3];
-            console.log( targets[1].backgroundImage )
-        }
-    }
+            targets[0].textContent = event.target.id.toUpperCase();
+            targets[0].style.backgroundImage = items[i][3];}}
+}
+
+function out_button( event ){
+    if ( event.target.id === "rock" ||
+         event.target.id === "paper" ||
+         event.target.id === "scissor"){
+            targets[1].textContent = "";
+            targets[1].style.backgroundImage = "none";}
+}
+
+function click_button( event ){
+    let computerHand = Math.floor(Math.random()*3)
+
+    targets[1].textContent = items[ computerHand ][0].id.toUpperCase();
+    targets[1].style.backgroundImage = items[ computerHand ][3];
+
+    // if ( items[ computerHand ][0] )
+    console.log( computerHand )
+}
+
+addEventListener("mouseover", function( event ){
+    over_button( event )
+})
+addEventListener("mouseout", function( event ){
+    out_button( event )
+})
+addEventListener("click", function( event ){
+    click_button( event )
 })
